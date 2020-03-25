7aee3b435f2b6059c0be0bdac7ec32d6230d794c
Fedor B Gorsky <fedor@support-pc.org>
2020-03-25 17:13:49 +0300

    Icons

--- src/main/java/ru/openitstudio/sshide/components/newsession/AutoScrollingJTree.java
+++ src/main/java/ru/openitstudio/sshide/components/newsession/AutoScrollingJTree.java
@@ -1,43 +1,55 @@
 package ru.openitstudio.sshide.components.newsession;
 
-import java.awt.Insets;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.dnd.Autoscroll;
+import ru.openitstudio.sshide.App;
 
-import javax.swing.JTree;
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeModel;
+import java.awt.*;
+import java.awt.dnd.Autoscroll;
 
 //http://www.java2s.com/Code/Java/Swing-JFC/DnDdraganddropJTreecode.htm
 public class AutoScrollingJTree extends JTree implements Autoscroll {
-	private int margin = 12;
+    private int margin = 12;
 
-	public AutoScrollingJTree() {
-		super();
-	}
+    public AutoScrollingJTree() {
+        super();
+        setIcons();
+    }
 
-	public AutoScrollingJTree(TreeModel model) {
-		super(model);
-	}
+    public AutoScrollingJTree(TreeModel model) {
+        super(model);
+        setIcons();
+    }
 
-	public void autoscroll(Point p) {
-		int realrow = getRowForLocation(p.x, p.y);
-		Rectangle outer = getBounds();
-		realrow = (p.y + outer.y <= margin ? realrow < 1 ? 0 : realrow - 1
-				: realrow < getRowCount() - 1 ? realrow + 1 : realrow);
-		scrollRowToVisible(realrow);
-	}
+    public void setIcons() {
+        DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) this.getCellRenderer();
+        Icon closedIcon = new ImageIcon(App.class.getResource("/icons/server-group.png"));
+        Icon openIcon = new ImageIcon(App.class.getResource("/icons/server-group.png"));
+        Icon leafIcon = new ImageIcon(App.class.getResource("/icons/server.png"));
+        renderer.setClosedIcon(closedIcon);
+        renderer.setOpenIcon(openIcon);
+        renderer.setLeafIcon(leafIcon);
+    }
 
-	public Insets getAutoscrollInsets() {
-		Rectangle outer = getBounds();
-		Rectangle inner = getParent().getBounds();
-		return new Insets(inner.y - outer.y + margin, inner.x - outer.x + margin,
-				outer.height - inner.height - inner.y + outer.y + margin,
-				outer.width - inner.width - inner.x + outer.x + margin);
-	}
+    public void autoscroll(Point p) {
+        int realrow = getRowForLocation(p.x, p.y);
+        Rectangle outer = getBounds();
+        realrow = (p.y + outer.y <= margin ? realrow < 1 ? 0 : realrow - 1
+                : realrow < getRowCount() - 1 ? realrow + 1 : realrow);
+        scrollRowToVisible(realrow);
+    }
 
-	// Use this method if you want to see the boundaries of the
-	// autoscroll active region
+    public Insets getAutoscrollInsets() {
+        Rectangle outer = getBounds();
+        Rectangle inner = getParent().getBounds();
+        return new Insets(inner.y - outer.y + margin, inner.x - outer.x + margin,
+                outer.height - inner.height - inner.y + outer.y + margin,
+                outer.width - inner.width - inner.x + outer.x + margin);
+    }
+
+    // Use this method if you want to see the boundaries of the
+    // autoscroll active region
 
 //	public void paintComponent(Graphics g) {
 //		super.paintComponent(g);
